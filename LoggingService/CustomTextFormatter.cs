454545fd50997aadedb32a2b8da2659281d5ad4f
MichalKobarynko <kobarynkomichal@gmail.com>
2023-02-13 19:29:38 +0100

    Dodaj pliki projektów.

---
+++ LoggingService/CustomTextFormatter.cs
@@ -0,0 +1,19 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingService
+{
+    public class CustomTextFormatter : ITextFormatter
+    {
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            
+        }
+    }
+}
