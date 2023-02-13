998d862c820424bc43ea411db91f22101bb98fc4
MichalKobarynko <kobarynkomichal@gmail.com>
2023-02-13 19:43:57 +0100

    Serwis Logujący Błędy/ dodanie klasy CustomTextForrmater

--- LoggingService/CustomTextFormatter.cs
+++ LoggingService/CustomTextFormatter.cs
@@ -13,7 +13,26 @@ namespace LoggingService
     {
         public void Format(LogEvent logEvent, TextWriter output)
         {
-            
+            if(logEvent.Level.ToString() != "Information")
+            {
+                output.WriteLine("--------------------------------------------------------------");
+                output.WriteLine($"Timestamp = {logEvent.Timestamp} | Level - {logEvent.Level} |");
+                output.WriteLine("--------------------------------------------------------------");
+                foreach(var item in logEvent.Properties)
+                {
+                    output.WriteLine(item.Key + " : " + item.Value);
+                }
+                if(logEvent.Exception != null)
+                {
+                    output.WriteLine("-------------------EXCEPTION DETAILS-------------------");
+                    output.WriteLine("Exception - {0}", logEvent.Exception);
+                    output.WriteLine("StackTrace - {0}", logEvent.Exception.StackTrace);
+                    output.WriteLine("Message - {0}", logEvent.Exception.Message);
+                    output.WriteLine("Source - {0}", logEvent.Exception.Source);
+                    output.WriteLine("InnerException - {0}", logEvent.Exception.InnerException);
+                }
+                output.WriteLine("--------------------------------------------------------------");
+            }
         }
     }
 }
